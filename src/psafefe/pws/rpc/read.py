e9acfac8225e4036dc0ba75a94a43f17fd0dedf7
Paulson McIntyre (GpMidi) <paul@gpmidi.net>
2012-07-13 21:37:23 -0400

    - Changed getInfoByDevice to only return info as device info style - Added getInfoListByDevice that returns a list of all entries in 'raw psafe entry' style

--- src/psafefe/pws/rpc/read.py
+++ src/psafefe/pws/rpc/read.py
@@ -66,9 +66,9 @@ def getInfoByUUID(username, password, locID, safeID, entryUUID, passwords, **kw)
     log.debug("Ran %r" % r)
     return r.wait()
     
-@rpcmethod(name = 'psafefe.pws.read.getInfoByDevice', signature = ['struct', 'string', 'string', 'int', 'int', 'string', 'array'])
+@rpcmethod(name = 'psafefe.pws.read.getInfoListByDevice', signature = ['struct', 'string', 'string', 'int', 'int', 'string', 'array'])
 @auth
-def getInfoByDevice(username, password, locID, safeID, device, passwords, **kw):
+def getInfoListByDevice(username, password, locID, safeID, device, passwords, **kw):
     """ Return struct representing the requested entries 
     @param username: Requesting user's login
     @type username: string
@@ -106,3 +106,66 @@ def getInfoByDevice(username, password, locID, safeID, device, passwords, **kw):
     log.debug("Ran %r" % r)
     return r.wait()
     
+@rpcmethod(name = 'psafefe.pws.read.getInfoByDevice', signature = ['struct', 'string', 'string', 'int', 'int', 'string', 'array'])
+@auth
+def getInfoByDevice(username, password, locID, safeID, device, passwords, **kw):
+    """ Return struct representing the requested entry. 
+    @param username: Requesting user's login
+    @type username: string
+    @param password: Requesting user's login
+    @type password: string
+    @param locID: PK of the psafe repo to use
+    @type locID: int
+    @param safeID: PK of the entry to return
+    @type safeID: int
+    @param device: Device FQDN
+    @type device: string/hostname
+    @param passwords: A list of passwords to use when decrypting psafes. 
+    @type passwords: list of strings   
+    @return: A dictionary containing the entities properties
+    @raise EntryDoesntExistError: The requested entry doesn't exist or the user doesn't have permission to read it.
+    @raise NoPermissionError: The user doesn't have enough perms to access the given loc
+    @raise InvalidIDError: The PK passed in wasn't found.
+    @note: The repo.updatePSafeList function may need to be run prior to using this function. The DB-based list of psafes and loc must exist and be up-to-date.  
+    """
+    try:
+        loc = PasswordSafeRepo.objects.get(pk = locID)
+    except Exception, e:
+        log.debug("Got %r trying to lookup pk=%r" % (e, locID))
+        raise InvalidIDError, "PK %r not found in repo list" % locID
+    if not loc.user_can_access(user = kw['user'], mode = 'R'):
+        raise NoPermissionError, "%r doens't have read-only access to the given loc" % username
+    try:
+        safe = PasswordSafe.objects.get(pk = safeID)
+    except Exception, e:
+        log.debug("Got %r trying to lookup pk=%r" % (e, safeID))
+        raise InvalidIDError, "PK %r not found in psafe list" % safeID
+    
+    # Now know the user should have access to it and that it's already known
+    r = lookupByDevice.delay(loc = loc.path, device = device, psafeLoc = safe.filename, passwords = passwords)
+    log.debug("Ran %r" % r)
+    infos = r.wait()
+    
+    ret = dict(Logins = {}, Info = {}, Other = {})
+    for uuid, entry in infos.items():
+        if entry.has_key('Group'):
+            ret['Group'] = entry['Group']
+        if entry.has_key("Title"):
+            if entry['Title'] == 'Logins':
+                log.debug("Found a login entry: %r" % entry)
+                ret['Logins'][entry['Username']] = entry['Password']
+                continue
+            elif entry['Title'] == 'Info':
+                log.debug("Found an Info entry: %r" % entry)
+                if entry['Password'] == "See Note":
+                    ret['Info'][entry['Username']] = entry['Note']
+                else:
+                    ret['Info'][entry['Username']] = entry['Password']
+                continue
+        log.debug("Found an unknown entry: %r" % entry)
+        if entry['Password'] == "See Note":
+            ret['Other'][entry['Username']] = entry['Note']
+        else:
+            ret['Other'][entry['Username']] = entry['Password']
+    
+    return ret
