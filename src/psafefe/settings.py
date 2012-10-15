9bc0e0c23cf98f20bac00ae38160be0380ae3393
Paulson McIntyre (GpMidi) <paul@gpmidi.net>
2012-10-14 22:31:47 -0400

    Changed DB loc to /tmp

--- src/psafefe/settings.py
+++ src/psafefe/settings.py
@@ -44,7 +44,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         # Change by day for the moment
-        'NAME': 'database.psafe3',  # Or path to database file if using sqlite3.
+        'NAME': '/tmp/database.psafe3',  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
